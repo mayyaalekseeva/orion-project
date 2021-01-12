6666ec4c728ae0688628218b8f587c0b4962cf5b
mayyaalekseeva <loth@mail.ru>
2021-01-13 02:42:38 +0300

    *

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+cd -
