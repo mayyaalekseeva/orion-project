43f43946947c2caa03f4ae110777035d2ae7ac74
mayyaalekseeva <loth@mail.ru>
2021-01-13 03:18:28 +0300

    *

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,7 @@ module.exports = {
 
         }
     },
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/' + process.env.CI_PROJECT_NAME + '/' :
-        '/'
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-project/'
+    : '/'
 }
