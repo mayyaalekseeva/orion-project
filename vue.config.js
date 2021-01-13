e4478819089824d2bfccb02b3942a7f2f35dca09
mayyaalekseeva <loth@mail.ru>
2021-01-13 03:43:52 +0300

    json

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,5 @@
 module.exports = {
-    devServer: {
-        open: true,
-        hot: true,
-        port: 8080,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                pathRewrite: {
-                    '^/api': ''
-                },
-                secure: false,
-                changeOrigin: true
-            }
-
-        }
-    },
-    publicPath: process.env.NODE_ENV === 'production'
+       publicPath: process.env.NODE_ENV === 'production'
     ? '/orion-project/'
     : '/'
 }
